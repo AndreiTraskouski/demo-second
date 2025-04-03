507c1f4e0db5c33377a256437ad48f658939e12e
Traskouski <and011@tut.by>
2025-04-03 21:15:48 +0300

    add variable i

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args){
+        int i=0;
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub");
         System.out.println("Hello GitHub");
